588a26c39acfb7677e515d28f60cb55c745c3b7f
Chapi <redakcja@koreja.pl>
2017-08-15 23:21:52 +0200

    Add view aplication object

--- js/app.js
+++ js/app.js
@@ -1,2 +1,30 @@
 (function(appContainerId) {
+
+/* VIEWS */
+
+	/**
+	 * Application View Object.
+	 * @constructor
+	 * @param {string} appContainerId - Aplication container ID.
+	 */
+
+	function AppObject(appContainerId) {
+		var	app = document.getElementById(appContainerId),
+			view = document.createElement('div'),
+			info = document.createElement('div');
+		
+		view.id = "view";
+		view.classList.add("view");
+		app.appendChild(view);	
+		
+		info.id = "info";
+		info.classList.add("info");
+		app.appendChild(info);	
+
+		this.view = view;
+		this.info = info;
+		this.app = app;	
+	}
+
+	var appObject = new AppObject(appContainerId);
 })('app');
